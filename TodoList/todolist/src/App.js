ba588d8bd22e741d6085baabddf58c0602337652
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-14 19:40:52 +1100

    add todo completed

---
+++ TodoList/todolist/src/App.js
@@ -0,0 +1,12 @@
+import { TodoWraper } from './components/TodoWrapper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWraper />
+    </div>
+  );
+}
+
+export default App;
